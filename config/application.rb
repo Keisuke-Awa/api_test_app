4e0ebae25fb290b08171a79f53e77009316eea62
keisukeawazu <keisuke.awa.basket@gmail.com>
2018-11-02 11:57:48 +0900

    許可ドメインの変更

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module ApiTestApp
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'http://localhost:8080'
         resource '*',
                  headers: :any,
                  methods: [:get, :post, :patch, :delete, :options]
